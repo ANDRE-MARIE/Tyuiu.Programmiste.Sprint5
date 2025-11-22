89ee1a12a4d7ff1d1c00ce3f34010157b623d158
GNOLA25-1 <agdmdot@gmail.com>
2025-11-22 13:13:28 +0500

    Task2

--- Tyuiu.Programmiste.Sprint5.Task2.V9/Program.cs
+++ Tyuiu.Programmiste.Sprint5.Task2.V9/Program.cs
@@ -1,20 +1,20 @@
-﻿
+﻿using System;
 using Tyuiu.Programmiste.Sprint5.Task2.V9.Lib;
+
 internal class Program
 {
-
     private static void Main(string[] args)
     {
         int[,] matrix = {
-                { 6, 8, 3 },
-                { 2, 6, 8 },
-                { 1, 7, 1 }
-            };
+            { 6, 8, 3 },
+            { 2, 6, 8 },
+            { 1, 7, 1 }
+        };
 
         MatrixProcessor processor = new MatrixProcessor();
         string filePath = processor.SaveToFileTextData(matrix);
 
         Console.WriteLine($"Fichier sauvegardé : {filePath}");
+        Console.WriteLine("Vérifiez dans le dossier 'output' si le fichier est correct.");
     }
 }
-
